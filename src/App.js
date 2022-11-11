6de9e8e104196199c1e8d36a5f3b10e39c35fdee
Julieta Magnago <julietamagnago@gmail.com>
2022-11-11 11:06:41 +0100

    first commit - functionality: sound on keypress and click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { DrumPad } from './DrumPad/DrumPad';
+import { DrumController } from './DrumController/DrumController';
+
+const audioClips = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Apoen-HH',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" 
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: 'Kick-n-Hat',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' 
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
+]
+
 
 function App() {
+  const [play, setPlay] = useState(false);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div id='drum-machine'>
+      <div id='drum-pad'>
+        {audioClips.map((clip) => {
+        return <DrumPad clip={clip} key={clip.keyTrigger} />
+        })}
+      </div>
+        <DrumController  />
+      </div>
+      
     </div>
   );
 }
---
+++ src/DrumController/DrumController.js
@@ -0,0 +1,11 @@
+import './DrumController.css';
+
+export function DrumController(props){
+    return(
+        <div id="controller">
+            <div id="nameDisplayer">
+                
+            </div>
+        </div>
+    )
+}
